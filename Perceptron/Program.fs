288e00984541cc66149f31a3dc3721c7eeccfd62
carbon_twelve <carbon.tetrachloride+github@gmail.com>
2012-10-19 01:54:40 +0900

    created project for perceptron

---
+++ Perceptron/Program.fs
@@ -0,0 +1,9 @@
+﻿// F# の詳細については、http://fsharp.net を参照してください
+// 詳細については、'F# チュートリアル' プロジェクトを参照してください。
+
+
+let perceptron ()
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    0 // 整数の終了コードを返します
